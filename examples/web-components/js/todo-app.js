87b438ba6efb35cdbf6b38108dd8e66ecf4f417e
Jonas George <jonas.george@outlook.com>
2020-03-07 14:26:36 +0100

    initialised package management, added first web component

--- examples/web-components/js/todo-app.js
+++ examples/web-components/js/todo-app.js
@@ -1,1 +1,25 @@
-console.log("hello todo mvc");
+import {html, render} from 'https://unpkg.com/lit-html?module';
+
+class TodoApp extends HTMLElement {
+    constructor() {
+        super();
+        this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+
+        render(this.template(), this._shadowRoot, {eventContext: this});
+
+        this.$input = this._shadowRoot.querySelector('input');
+    }
+
+    template() {
+        return html`
+            <section class="todoapp">
+                <header class="header">
+                    <h1>todos</h1>
+                    <input class="new-todo" placeholder="What needs to be done?" autofocus>
+                </header>
+            </section>
+        `;
+    }
+}
+
+window.customElements.define('todo-app', TodoApp);
